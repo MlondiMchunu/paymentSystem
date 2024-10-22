ecccca872b904dc65bfd6a7a73c40e430fed9060
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-22 17:03:56 +0200

    mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const app = express()
 
 //specify  port from environment variable
 const PORT = process.env.PORT ||8080
@@ -8,7 +9,8 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/',async(req,res)=>{
-    res.send("Hello Payment")
+   // res.send("Hello Payment")
+    res.json({message:"Hello from server"})
 })
 
 app.listen(PORT,()=>{
