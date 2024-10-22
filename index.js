f4ed022b1012395e875eb04cf5c0716f7e4f5f6f
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-22 15:59:59 +0200

    specify port and use middleware

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+
+//specify  port from environment variable
+const PORT = process.env.PORT ||8080
+app.use(cors())
+app.use(express.json())
