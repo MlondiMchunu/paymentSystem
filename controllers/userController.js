2bbfbde5bca2efa436d9ae36f340bbf67097bec3
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-23 12:03:38 +0200

    fetch user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,18 @@ const createUser = async(req,res)=>{
         status: 0,
     })
 }
+
+const getUser = async(req,res)=>{
+    try{
+        let {id} = req.params
+        const user = await User.findById(id)
+
+        res.status(200).send({
+            user,
+            message: "Found user Details",
+            status: 0,
+        })
+    }catch(err){
+        res.status(500).send({data:{}, error: err.message, status: 1})
+    }
+}
