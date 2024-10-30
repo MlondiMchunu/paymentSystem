c5ceb5e7e92d6937511a314b2f13695aeb34a3f5
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-10-30 03:30:51 +0200

    latest commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const userRoute = express.Router()
 
-const { getUser,getUsers, createUser, initializeTrans } = require("../controllers/userController")
+const { getUser,
+    getUsers, 
+    createUser, 
+    initializeTrans,
+    verifyTrans } = require("../controllers/userController")
 
 userRoute.get('/getUser/:id',getUser)
 userRoute.get('/getUsers',getUsers)
 userRoute.post('/createUser',createUser)
 userRoute.post('/transact/:id',initializeTrans)
+userRoute.post("/verifyTrans/:id",verifyTrans)
 
 module.exports = {userRoute}
